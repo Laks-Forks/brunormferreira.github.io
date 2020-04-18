ce1790f09d5f9a600dec4739ff06714631714944
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-17 21:03:45 -0300

    adding initial styles and libs

--- build.js
+++ build.js
@@ -1,5 +1,9 @@
-var fs = require("fs");
-var yamlFront = require("yaml-front-matter");
+// ======================================================================
+// THIS FILE BASICALLY GET MARDOWN DATA YAML AND CONVERT TO .JSON FILE
+// ======================================================================
+
+let fs = require("fs");
+let yamlFront = require("yaml-front-matter");
 
 fs.readdirSync("./content").forEach((file) => {
   fs.readFile(`./content/${file}`, "utf8", function (err, data) {
